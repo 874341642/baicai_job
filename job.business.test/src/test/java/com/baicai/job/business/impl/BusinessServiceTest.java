ed15d1e1522cb5002a70870229e0537be545187d
yuezu1026 <39002818@qq.com>
2017-03-26 15:09:24 +0800

    add

--- job.business.test/src/test/java/com/baicai/job/business/impl/BusinessServiceTest.java
+++ job.business.test/src/test/java/com/baicai/job/business/impl/BusinessServiceTest.java
@@ -22,7 +22,7 @@ public class BusinessServiceTest extends BaseJunit4Test{
 @Test   //标明是测试方法  
 @Transactional   //标明此方法需使用事务  
 @Rollback(false)  //标明使用完此方法后事务不回滚,true时为回滚  
-public void testFindUser( ) {  
+public void testFindUser( ) {   
        CustomerUser customerUser=testDao.selectByPrimaryKey("1");  
        System.out.println(customerUser);  
        Gson gson = new Gson();
