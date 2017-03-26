904af5a176aad18b2e5ac5dd77068fab48548ba0
yuezu1026 <39002818@qq.com>
2017-03-26 16:31:03 +0800

    add

--- job.member.api/src/main/java/com/baicai/job/member/login/service/IMemberService.java
+++ job.member.api/src/main/java/com/baicai/job/member/login/service/IMemberService.java
@@ -1,11 +1,13 @@
 package com.baicai.job.member.login.service;
 
+import org.omg.CORBA.portable.ApplicationException;
+
 /** @Package com.baicai.job.member.api  
  * @ClassName IMemberService  
  * @author 刘小军
  * @date 2017年3月25日 下午6:05:13    
  */
 public interface IMemberService {
-	public  Boolean  memberLogin(String account,String password);
+	public  Boolean  memberLogin(String account,String password) throws ApplicationException;
 	
 }
