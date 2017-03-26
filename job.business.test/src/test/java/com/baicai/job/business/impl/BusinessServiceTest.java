f83ef7df63dd253e6b45c6a9c0931519f26ae6c0
yuezu1026 <39002818@qq.com>
2017-03-26 15:37:29 +0800

    add

--- job.business.test/src/test/java/com/baicai/job/business/impl/BusinessServiceTest.java
+++ job.business.test/src/test/java/com/baicai/job/business/impl/BusinessServiceTest.java
@@ -13,21 +13,21 @@ import com.baicai.job.business.test.vo.CustomerUser;
 import com.baicai.job.test.common.BaseJunit4Test;
 import com.google.gson.Gson;
 
-public class BusinessServiceTest extends BaseJunit4Test{
-	
-	
-	@Inject  //自动注入,默认按名称  
-    private ITestDao testDao;   
- 
-@Test   //标明是测试方法  
-@Transactional   //标明此方法需使用事务  
-@Rollback(false)  //标明使用完此方法后事务不回滚,true时为回滚  
-public void testFindUser( ) {   
-       CustomerUser customerUser=testDao.selectByPrimaryKey("1");  
-       System.out.println(customerUser);  
-       Gson gson = new Gson();
-       gson.toJson(customerUser);
-       assertTrue( customerUser != null);   
-    }  
+public class BusinessServiceTest extends BaseJunit4Test {
+
+	@Inject // 自动注入,默认按名称
+	private ITestDao testDao;
+
+	@Test // 标明是测试方法
+	@Transactional // 标明此方法需使用事务
+	@Rollback(false) // 标明使用完此方法后事务不回滚,true时为回滚
+	public void testFindUser() {
+		CustomerUser customerUser = testDao.selectByPrimaryKey("1");
+		System.out.println(customerUser);
+		Gson gson = new Gson();
+		gson.toJson(customerUser);
+		assertTrue(customerUser != null);
+	}
+
 
 }
